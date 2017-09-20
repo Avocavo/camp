4554f657391fe0a1f9db9ef154b6749f138aadb8
Anubis <anubis@Anubiss-MacBook-Pro.local>
2017-09-20 19:08:19 -0400

    Setup react-toolbox

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Button } from 'react-toolbox/lib/button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <p>TEST</p>
+        <Button label="Hello World!" />
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   entry: __dirname + '/src/index.js',
   output: { path: __dirname + '/pub', filename: 'bundle.js' },
+  resolve: {
+    extensions: [".js", ".json", ".css"]
+  },
+  //devtool: "eval-source-map", //not sure??
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?$/,  //compiles js or jsx
         exclude: /node_modules/,
@@ -10,6 +14,22 @@ module.exports = {
         query: {
           presets:['react'] //move to .babelrc?
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: true,
+              importLoaders: 1,
+              localIdentName: "[name]--[local]--[hash:base64:8]"
+            }
+          },
+          "postcss-loader" // has separate config, see postcss.config.js nearby
+        ]
       }
     ]
   }
