c1b7f8ec7eaee9f80bf04c7d9c03c58f48e796c1
Anubis <anubis@Anubiss-MacBook-Pro.local>
2017-09-20 22:08:39 -0400

    Create test layout with react-toolbox

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import ButtonTest from './ButtonTest';
-import RadioTest from './RadioTest';
+//import ButtonTest from './ButtonTest';
+//import RadioTest from './RadioTest';
+import LayoutTest from './layouts/LayoutTest';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>TEST</p>
-        <ButtonTest />
-        <RadioTest />
+        <LayoutTest />
       </div>
     );
   }
